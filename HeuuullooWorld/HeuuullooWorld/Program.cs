8740d288da5ed3c36ef4e92fa51b123bfd3c76a4
alexanderrost <alexander.rost@hotmail.com>
2017-01-19 09:34:54 +0100

    Add files via upload

---
+++ HeuuullooWorld/HeuuullooWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeuuullooWorld
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            TestWrite writeHW = new TestWrite();
+
+            writeHW.Run();
+            Console.ReadLine();
+        }
+    }
+}
